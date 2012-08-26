a356c98b46201fbe51204e6a913ab2916d5e5612
Vicente Mundim <vicente.mundim@gmail.com>
2012-08-26 15:27:15 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EstampadaCom::Application.config.secret_token = 'ff2bdaaf8ce2f1aeee636209be56eb29f8bb3406192f4bf1ef9d527da0ea20bec196b4208bd5facb1a9858f72c37169464c410d9f509e69c3fd1ea19c1312642'
