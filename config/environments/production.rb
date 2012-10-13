3f4b607301ec46440c08ae0fa655f644e0737eef
Vicente Mundim <vicente.mundim@gmail.com>
2012-10-13 14:26:15 -0300

    Properly configuring carrierwave to save images on S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ EstampadaCom::Application.configure do
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']    # required
     }
     config.fog_directory  = ENV['S3_BUCKET']                        # required
-    config.fog_public     = false                                   # optional, defaults to true
+    config.fog_public     = true                                    # optional, defaults to true
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
   end
 end
